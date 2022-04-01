50368c3a30c0ad188115c1d74c30b522731820e9
TAKERU-KURIYAMA <takerukuriyama@gmail.com>
2022-04-01 02:13:34 +0000

    4/1/11:12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,42 +1,52 @@
 class BooksController < ApplicationController
- 
+
   def new
     @book = Book.new
   end
-  
+
   def index
     @book = Book.new
     @books = Book.all
   end
-  
+
   def show
     @book = Book.new
-    @bookf = Book.find(params[:id])  
+    @bookf = Book.find(params[:id])
   end
-  
+
   def edit
-    @book = Book.find(params[:id])  
+    @book = Book.find(params[:id])
   end
-  
+
   def create
     @book = Book.new(book_params)
+    @books = Book.all
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "You have created book successfully."
+    else
+      render :index
+    end
   end
-  
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to '/books'
   end
-  
+
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book)  
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end  
+    
   end
-  
+
   private
 
   def book_params
